13ce4dbd690ad0932ea893f77ee7e8cda34c407e
ivan Jankovic <jankoemail@gmail.com>
2019-03-29 11:56:50 -0400

    Updates

---
+++ precache-manifest.b1105682bff7daa4f633477a021e7cea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "43751571f02a8e70402d9ec122f471e0",
+    "url": "/maxx-potential-web-lab/static/media/hero1.43751571.jpg"
+  },
+  {
+    "revision": "7de8295caaf5442b8d09",
+    "url": "/maxx-potential-web-lab/static/js/runtime~main.f451c782.js"
+  },
+  {
+    "revision": "1e5fd13eeb14b432d8f4",
+    "url": "/maxx-potential-web-lab/static/js/main.cd51de6d.chunk.js"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/maxx-potential-web-lab/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "1e5fd13eeb14b432d8f4",
+    "url": "/maxx-potential-web-lab/static/css/main.521c5a45.chunk.css"
+  },
+  {
+    "revision": "0c0fbc435f4af60f1f5a406ba2f93094",
+    "url": "/maxx-potential-web-lab/index.html"
+  }
+];
